840b65268039be66522def2e2fe49600a1208bea
vishnumahamkali <v.mahamkali@uq.edu.au>
2020-05-17 21:21:01 +1000

    constraints update

--- multiTFA/util/constraints.py
+++ multiTFA/util/constraints.py
@@ -1,5 +1,5 @@
 from six import iteritems
-from numpy import log, isnan, where, delete, array, linalg, sqrt, diag, shape
+from numpy import log, isnan, where, delete, array, linalg, sqrt, diag, shape, count_nonzero, square, zeros
 from  .thermo_constants import Vmax, RT, K
 from scipy.stats import chi2
 from .posdef import nearestPD, isPD
@@ -54,9 +54,15 @@ def metabolite_variables(metabolite):
                             'Ci_{}'.format(metabolite.id),
                             lb = -1.96, ub = 1.96)
         
+        if isnan(metabolite.delG_f):
+            lb_met = -100
+            ub_met = 100
+        else:
+            lb_met = metabolite.delG_f -100
+            ub_met = metabolite.delG_f + 100
         met_variable = metabolite.model.problem.Variable(
                             'met_{}'.format(metabolite.id),
-                            lb = -100000, ub = 100000)
+                            lb = lb_met, ub = ub_met)
         return conc_variable, Ci_variable, met_variable
     else:
         return None
@@ -202,53 +208,57 @@ def MIQP(model):
 
     
     # Problem metabolites, if met.delGf == 0 then delete it, if S.D > 100 make a sepearate covariance matrix
-    problem_indices = []
-    problem_mets = []
     delete_ind = []
     delete_met = []
+    cov_mets = []
+    cov_met_ids = []
     for met in model.metabolites:
-        if met.delG_f == 0 or isnan(met.delG_f):
+        if count_nonzero(model.cholskey_matrix[:,model.metabolites.index(met)]) == 0 \
+                         or isnan(met.delG_f):
             delete_ind.append(model.metabolites.index(met))
             delete_met.append(met)
-        elif sqrt(diag(model.cov_dG)[model.metabolites.index(met)]) > 25:
-            delete_ind.append(model.metabolites.index(met))
-            problem_indices.append(model.metabolites.index(met))
-            problem_mets.append(met)
-            delete_met.append(met)
+        #elif sqrt(diag(model.cov_dG)[model.metabolites.index(met)]) > 10:
+        #    delete_ind.append(model.metabolites.index(met))
+        #    problem_indices.append(model.metabolites.index(met))
+        #    problem_mets.append(met)
+        #    delete_met.append(met)
         else:
-            continue
+            cov_met_ids.append(model.metabolites.index(met))
+            cov_mets.append(met.id)
 
-    print(delete_met)
+    #print(delete_met)
     # First construct high variance matrix then proceed to the normal one
     cov_dg = model.cov_dG
 
     # First pick high variance columns from cov_dG and delete the rows corresponding to non problem metabolites
-    whole_ind = [ind for ind in range(len(cov_dg))]
-    non_problem = list(set(whole_ind).difference(set(problem_indices)))
-    large_covar = cov_dg[:,problem_indices]
-    large_covar = delete(large_covar, non_problem, axis = 0)
+    #whole_ind = [ind for ind in range(len(cov_dg))]
+    #non_problem = list(set(whole_ind).difference(set(problem_indices)))
+    #large_covar = cov_dg[:,problem_indices]
+    #large_covar = delete(large_covar, non_problem, axis = 0)
 
     # Now construct the normal covariance matrix
-    cov_dg = delete(cov_dg, delete_ind, axis=0)
-    cov_dg = delete(cov_dg, delete_ind, axis=1)
+    cov_dG = cov_dg[:,cov_met_ids]
+    cov_dg = cov_dG[cov_met_ids, :]
 
     # Now check if both normal and high covar matrices are posdef and calculate inverse
     if not isPD(cov_dg):
         nearPD = nearestPD(cov_dg)
         inv_cov = linalg.inv(nearPD)
     else:
-        inv_cov = linalg.inv(cov_dg)
+        nearPD = cov_dg
+        inv_cov = linalg.inv(nearPD)
 
-    if not isPD(large_covar):
+    """if not isPD(large_covar):
         nearPD_lc = nearestPD(large_covar)
         inv_cov_lc = linalg.inv(nearPD_lc)
     else:
-        inv_cov_lc = linalg.inv(large_covar)    
-
-    
+        inv_cov_lc = linalg.inv(large_covar)  
+    """
     # Separate chi-square values for both covariances
     chi_crit_val = chi2.isf(q = 0.05, df = len(cov_dg))
-    chi_crit_val_lc = chi2.isf(q = 0.05, df = len(large_covar))
+    #chi_crit_val_lc = chi2.isf(q = 0.05, df = len(large_covar))  
+
+    bounds = bounds_ellipsoid(nearPD)
 
     #Construct the qc variable matrices for both cases 
     
@@ -258,6 +268,10 @@ def MIQP(model):
     for met in model.metabolites:
         if met in delete_met:
             continue
+        metid_grbvars_dict[met.id].LB = met.delG_f \
+                                - bounds[cov_mets.index(met.id)]
+        metid_grbvars_dict[met.id].UB = met.delG_f \
+                                + bounds[cov_mets.index(met.id)]
         met_var.append(metid_grbvars_dict[met.id])
         delG_mean.append(met.delG_f)
     met_var = array(met_var)
@@ -265,12 +279,13 @@ def MIQP(model):
     mu_var = met_var - delG_mean
     mu_var = array([mu_var])
 
-    print(shape(inv_cov))
+    #print(shape(inv_cov))
     lhs = mu_var @ inv_cov @ mu_var.T    
     cons = lhs[0]
     gurobi_interface.addQConstr(cons[0] <= chi_crit_val, "qp_constraint")
     gurobi_interface.update()
 
+    """
     # For high variance 
     met_var_lc = []
     for met in problem_mets:
@@ -282,8 +297,41 @@ def MIQP(model):
     cons_lc = lhs_lc[0]
     #gurobi_interface.addQConstr(cons_lc[0] <= chi_crit_val_lc,                                              "qp_constraint_lc")
     #gurobi_interface.update()
+    """
     print('done')
 
     gurobi_interface.write('test.lp')
 
     return gurobi_interface
+
+
+def bounds_ellipsoid(covariance):
+    """ Calculates the bounds of formation energy variables from the covariance matrix. It will help the solver to get to the solution space quick, for the quadratic constraint problem. 
+
+    Half length of ellipse axis is sqrt(eigen_val * chisquare_val). We calculate unit eigen vectors and multiply my corresponding half length. reconstruct the matrix and pick the largest value of each row, that should define the upper bound for the metabolite formation energy.
+
+
+    :param covariance: [description]
+    :type covariance: [type]
+    :return: [description]
+    :rtype: [type]
+    """
+
+    # First calculate the half lengths of ellipsoid
+    chi2_value = chi2.isf(q = 0.05, df = len(covariance))
+    eig_val, eig_vec = linalg.eig(covariance)
+    half_len = sqrt(chi2_value * eig_val)
+
+    # Calculate unit eigen vectors and UB in various axis for formation energies
+    bounds_mat = zeros((len(covariance), len(covariance)))
+
+    for i in range(len(eig_vec)):
+        scaling = sqrt(sum(square(eig_vec[:,i])))
+        unit_vec = eig_vec[:,i]/scaling
+        bounds_mat[:,i] = half_len[i] * unit_vec
+
+    UB = []
+    for i in range(len(bounds_mat)):
+        UB.append(max(bounds_mat[i,:]))
+
+    return UB
