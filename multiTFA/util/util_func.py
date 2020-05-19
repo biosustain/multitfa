08c6285db7c98a5a223c049118acf394904b54e0
vishnumahamkali <vishnumahamkali@gmail.com>
2020-05-19 13:09:29 +1000

    correlation

--- multiTFA/util/util_func.py
+++ multiTFA/util/util_func.py
@@ -13,3 +13,42 @@ def add_method(cls):
         # Note we are not binding func, but wrapper which accepts self but does exactly the same as func
         return func # returning func means func can still be used normally
     return decorator
+
+import numpy as np
+from .posdef import isPD, nearestPD
+
+def cov2corr(covariance):
+    """ Calculates correlation matrix from covariance matrix
+    corr(i,j) = cov(i,j)/stdev(i) * stdev(j) 
+
+    Arguments:
+        covariance {np.ndarray} -- covariance matrix
+
+    Returns:
+        [np.ndarray] -- correlation matrix
+    """
+    
+    stdev = np.sqrt(np.diag(covariance))
+    outer_stdev = np.outer(stdev, stdev)
+    correlation = covariance / outer_stdev
+    correlation[covariance == 0] = 0
+    
+    return correlation
+
+
+def findcorrelatedmets(covariance):
+    non_prob_ind = []
+    prob_ind = []
+    for i in range(len(covariance)):
+        if not np.isnan(covariance[:,i]).all():
+            non_prob_ind.append(i)
+        else:
+            prob_ind.append(i)
+
+    reduced_covar = covariance[:, non_prob_ind]
+    reduced_covar = reduced_covar[non_prob_ind, :]
+
+    correlation_mat = cov2corr(covariance)
+
+    return correlation_mat
+    
