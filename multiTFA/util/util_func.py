714e1eb0ef656895614eb318bb0cce4bac6e7bf0
vishnumahamkali <vishnumahamkali@gmail.com>
2020-05-21 11:49:25 +1000

    correlation coefficient

--- multiTFA/util/util_func.py
+++ multiTFA/util/util_func.py
@@ -36,19 +36,51 @@ def cov2corr(covariance):
     return correlation
 
 
-def findcorrelatedmets(covariance):
-    non_prob_ind = []
-    prob_ind = []
-    for i in range(len(covariance)):
-        if not np.isnan(covariance[:,i]).all():
-            non_prob_ind.append(i)
-        else:
-            prob_ind.append(i)
-
-    reduced_covar = covariance[:, non_prob_ind]
-    reduced_covar = reduced_covar[non_prob_ind, :]
-
+def findcorrelatedmets(model):
+    
+    covariance = model.cov_dG
     correlation_mat = cov2corr(covariance)
 
-    return correlation_mat
+    # Check for nan in correlation matrix and keep track of them and the corresponding metabolites
+    non_prob_ind = []
+    prob_ind = []
+    non_prob_mets = []
+    nan_mets = []
+    for i in range(len(correlation_mat)):
+        if not np.isnan(correlation_mat[:,i]).all():
+            non_prob_ind.append(i)
+            non_prob_mets.append(model.metabolites[i].id)
+        else:
+            prob_ind.append(i)
+            nan_mets.append(model.metabolites[i].id)
+
+    reduced_correlation = correlation_mat[:, non_prob_ind]
+    reduced_correlation = reduced_correlation[non_prob_ind, :]
+
+    reduced_cov = covariance[:, non_prob_ind]
+    reduced_cov = reduced_cov[non_prob_ind, :]
+
+    #removing metabolites with all zeros in correlation matrix
+    zero_mets = []
+    final_mets = []
+    non_zero_ind = []
+
+    for i in range(0,len(reduced_correlation)):
+        if np.count_nonzero(reduced_correlation[:,i]) == 0:
+            zero_mets.append(non_prob_mets[i])
+        else:
+            non_zero_ind.append(i)
+            final_mets.append(non_prob_mets[i])
+
+    final_correlation = reduced_correlation[:, non_zero_ind]
+    final_correlation = final_correlation[non_zero_ind, :]
+
+    final_cov = reduced_cov[:, non_zero_ind]
+    final_cov = final_cov[non_zero_ind, :]
+    
+    # Find high variance metabolites
+    ind_high_variances = list(np.where(np.sqrt(np.diag(final_cov)) > 10 )[0]) 
+    high_var_mets = [final_mets[i] for i in ind_high_variances]
+
+    return final_correlation, final_mets, high_var_mets, ind_high_variances
     
