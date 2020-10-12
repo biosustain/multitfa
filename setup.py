0aaebe2d102ceb3881489b4046aded3216a8956b
Midnighter <midnighter@posteo.net>
2020-10-13 00:49:49 +0200

    chore: add all new config files

--- setup.py
+++ setup.py
@@ -1,49 +1,27 @@
-""" Thermodynamics-based Flux Analysis
-
-.. moduleauthor:: vishnu
+#!/usr/bin/env python
 
 
-"""
+# Copyright (c) 2018, Vishnuvardhan Mahamkali.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-from setuptools import setup, find_packages
 
-version_tag = "0.1.0"
+"""Set up the multivariate tMFA package."""
 
-setup(
-    name="multiTFA",
-    version=version_tag,
-    author="vishnu",
-    author_email="v.mahamkali@uq.edu.au",
-    url="https://github.com/biosustain/tmfa",
-    download_url="https://github.com/EPFL-LCSB/pytfa/archive/"
-    + version_tag
-    + ".tar.gz",
-    install_requires=["cobra>0.13", "optlang", "pytest", "scipy", "numpy"],
-    packages=find_packages(),
-    python_requires=">=3.6, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, <4",
-    description="multiTFA, Thermodynamics-based Flux Analysis in Python",
-    keywords=["multiTFA", "tfa", "thermodynamics", "flux analysis"],
-    license="Apache 2.0",
-    include_package_data=True,
-    package_data={"multiTFA": ["Data/*.pickle", "Data/*.map", "Data/*.npz"]},
-    # See https://PyPI.python.org/PyPI?%3Aaction=list_classifiers
-    classifiers=[
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
-        "Development Status :: 4 - Beta",
-        # Indicate who your project is intended for
-        "Intended Audience :: Science/Research",
-        "Topic :: Scientific/Engineering :: Bio-Informatics",
-        "Environment :: Console",
-        # Pick your license as you wish (should match "license" above)
-        "License :: OSI Approved :: Apache Software License",
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-    ],
-)
 
+import versioneer
+from setuptools import setup
+
+
+# All other arguments are defined in `setup.cfg`.
+setup(version=versioneer.get_version(), cmdclass=versioneer.get_cmdclass())
