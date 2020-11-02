0554bdbcd0ee73419f454c4c5db183a13f11a678
vishnumahamkali <vishnumahamkali@gmail.com>
2020-11-02 17:04:19 +1000

    doc string

--- multiTFA/core/reaction.py
+++ multiTFA/core/reaction.py
@@ -12,35 +12,20 @@ import numpy as np
 
 class thermo_reaction(Reaction):
 
-    """ Class representation of thermo reaction Object
+    """ Class representation of thermo reaction Object. We calculate the required thermodynamic constraints for performing tMFA. To do the constraints, we need Gibbs energy of reaction and transport.
 
-    Attributes:
-        All cobra.Reaction attributes
-        transport_metabolites: Metabolites involved in transport
-        transport_delG: Gibbs free energy of transport
-        delG_transform: transformed Gibbs free energy of reaction
-        S_matrix: stoichiometry of reaction (matrix form)
+        Parameters
+        ----------
+        cobra_rxn : cobra.core.Reaction
+            Cobra reaction object, to copy the attributes from. We copy metabolites and genes.
+        updated_model : core.tmodel, optional
+            tmodel object, with updated thermo properties, by default None
+    
     """
 
     def __init__(
         self, cobra_rxn, updated_model=None,
     ):
-
-        """
-        Arguments:
-            cobra_rxn {Cobra reaction object} -- Cobra model reaction object
-        
-        Keyword Arguments:
-            updated_model {tmodel} -- thermomodel object (default: {None})
-            Kegg_map {dict} -- Kegg map of metabolites (default: {{}})
-            pH_I_T_dict {dict} -- Dictionary of pH, I of compartments (default: {{}})
-            del_psi_dict {dict} -- Dictionary of membrane potential of compartments (default: {{}})
-            isTrans {bool} -- is it a trsnaport reaction (default: {False})
-            rxn_delG {int} -- standard delG of reaction (default: {0})
-            transport_delG {int} -- transport delG (default: {0})
-            concentration_dict {dict} -- Dictionary of concentration of metabolites (default: {{'min':{},'max':{}}})
-            stoichiometric_matrix {list} --  (default: {[]})
-        """
 
         self._model = updated_model
         do_not_copy_by_ref = {"_model", "_metabolites", "_genes"}
@@ -61,6 +46,13 @@ class thermo_reaction(Reaction):
 
     @property
     def delG_forward(self):
+        """ An optlang variable representing the Gibbs energy of the forward reaction. 
+
+        Returns
+        -------
+        optlang.interface.variable
+            An optlang variable for the Gibbs energy of the forward half reaction or None if the reaction is not associated with the model or reaction is in the list of thermodynamic excluded reactions.
+        """
         var_name = "dG_{}".format(self.forward_variable.name)
         if self.model is not None:
             if self.id not in self.model.Exclude_reactions:
@@ -72,6 +64,13 @@ class thermo_reaction(Reaction):
 
     @property
     def delG_reverse(self):
+        """An optlang variable representing the Gibbs energy of the reverse reaction.
+
+        Returns
+        -------
+        optlang.interface.variable
+            An optlang variable for the Gibbs energy of the reverse half reaction or None if the reaction is not associated with the model or reaction is in the list of thermodynamic excluded reactions.
+        """
         var_name = "dG_{}".format(self.reverse_variable.name)
         if self.model is not None:
             if self.id not in self.model.Exclude_reactions:
@@ -83,6 +82,13 @@ class thermo_reaction(Reaction):
 
     @property
     def indicator_forward(self):
+        """An optlang binary variable to dictate the flux through forward half reaction based on Gibbs energy.
+
+        Returns
+        -------
+        optlang.interface.variable
+            An optlang binary variable of forward half reaction or None if the reaction is not associated with the model or eaction is in the list of thermodynamic excluded reactions.
+        """
         var_name = "indicator_{}".format(self.forward_variable.name)
         if self.model is not None:
             if self.id not in self.model.Exclude_reactions:
@@ -94,6 +100,13 @@ class thermo_reaction(Reaction):
 
     @property
     def indicator_reverse(self):
+        """An optlang binary variable to dictate the flux through reverse half reaction based on Gibbs energy.
+
+        Returns
+        -------
+        optlang.interface.variable
+            An optlang binary variable of reverse half reaction or None if the reaction is not associated with the model or eaction is in the list of thermodynamic excluded reactions.
+        """
         var_name = "indicator_{}".format(self.reverse_variable.name)
         if self.model is not None:
             if self.id not in self.model.Exclude_reactions:
@@ -105,6 +118,15 @@ class thermo_reaction(Reaction):
 
     @property
     def directionality_constraint(self):
+        """optlang constraints to determine reaction directionlity based on the binary variable. The reaction is split is forward and reverse variables. Either forward or reverse variables can carry positive flux only if the corresponding binary variable is '1' which will be determined by the Gibbs energy. If binary variable is '0', corresponding flux variable will be '0'.
+
+        vi -Vmax * Zi <=0
+
+        Returns
+        -------
+        tuple 
+            tuple of optlang.interface.constraint of forward and reverse reaction directionalities. if the reaction is not associated with model or reaction present in list of thermodynamic excluded reactions, then None
+        """
         forward_cons = "directionality_{}".format(self.forward_variable.name)
         reverse_cons = "directionality_{}".format(self.reverse_variable.name)
 
@@ -121,6 +143,15 @@ class thermo_reaction(Reaction):
 
     @property
     def indicator_constraint(self):
+        """optlang constraint to ensure that reaction Gibbs energy is always < 0. 
+
+        delG -K + K*Zi <= 0
+
+        Returns
+        -------
+        tuple
+            tuple of optlang.interface.constraint of forward and reverse reaction indicator constraints. if the reaction is not associated with model or reaction present in list of thermodynamic excluded reactions, then None
+        """
         forward_cons = "ind_{}".format(self.forward_variable.name)
         reverse_cons = "ind_{}".format(self.reverse_variable.name)
 
@@ -137,6 +168,17 @@ class thermo_reaction(Reaction):
 
     @property
     def delG_constraint(self):
+        """optlang constraints to calculate the Gibbs energy of reactions. Forward and reverse reaction variables are represented separately.
+
+        delGr - RT * S.T * ln(x) - S.T * delGferr - S.T @ delGf0 - delGtransport = 0
+
+        Gibbs energy of a reaction is a linear combination of Gibbs energy of formation of metabolites at the compartment conditions and metabolite concentrations. There's an uncertainity associated with formation energy estimations. This uncertainity is modelled as error variable and is allowed to vary 2 standard devaitions from mean. Since the error is associated with formation energies they cancel out common error in a reaction.
+
+        Returns
+        -------
+        tuple
+            tuple of optlang.interface.constraint of forward and reverse reaction Gibbs energy constraints. if the reaction is not associated with model or reaction present in list of thermodynamic excluded reactions, then None
+        """
         forward_cons = "delG_{}".format(self.forward_variable.name)
         reverse_cons = "delG_{}".format(self.reverse_variable.name)
 
@@ -153,6 +195,13 @@ class thermo_reaction(Reaction):
 
     @property
     def delG_prime(self):
+        """Transformed Gibbs energy of a reaction. Standard Gibbs energy adjusted to the compartment conditions.
+
+        Returns
+        -------
+        float
+            transformed Gibbs energy of the reaction
+        """
         try:
             return self._delG_prime
         except AttributeError:
@@ -165,6 +214,13 @@ class thermo_reaction(Reaction):
 
     @property
     def delG_transport(self):
+        """Gibbs energy of transport of the reaction. 
+
+        Returns
+        -------
+        float
+            Gibbs energy of transport of the reaction
+        """
         try:
             return self._delG_transport
         except AttributeError:
@@ -176,7 +232,13 @@ class thermo_reaction(Reaction):
         self._delG_transport = value  # Update the consraints
 
     def calculate_transport_charge(self):
+        """calculates the net charge and protons transported across the membrane. Net charge and protons transported for each compartment should be same. This function assumes that transport reactions involve only two compartments. Any reaction involving more than 2 compartments are not necessarily true transport reactions like biomass or lumped reaction, which are not really useful for transport considerations.
 
+        Returns
+        -------
+        tuple
+            tuple of two dictionaries. Dictionaries of transport charge and protons for each compartment. 
+        """
         n_charge = {}
         n_proton = {}
         # print("lol1")
@@ -201,6 +263,13 @@ class thermo_reaction(Reaction):
         )
 
     def calculate_delG_transport(self):
+        """Gibbs energy of transport has two components, 1) proton transport across the membrane, This needs to be adjusted for compartment specific pH 2) charge transport, which is affected by membrane potential.
+
+        Returns
+        -------
+        float
+            Gibbs energy of transport
+        """
         if len(self.compartments) != 2:
             return 0
         else:
@@ -226,12 +295,13 @@ class thermo_reaction(Reaction):
             return electro_static_delG + proton_potential_adjustment
 
     def cal_stoichiometric_matrix(self):
-        """ Reaction stoichiometry, two columns to represent forward and reverse
-        
-        Returns:
-            np.ndarray -- S matrix of reaction
-        """
+        """stoichiometric vector of forward reaction. Vector has length of number of metabolites in the model.
 
+        Returns
+        -------
+        np.ndarray
+            numpy vector of reaction stoichiometry
+        """
         S_matrix = zeros((len(self.model.metabolites)))
         for metabolite, stoic in iteritems(self.metabolites):
             S_matrix[self.model.metabolites.index(metabolite)] = stoic
@@ -242,13 +312,13 @@ class thermo_reaction(Reaction):
         return self.metabolites[_id_to_metabolites[metabolite_id]]
 
     def calculate_delG_prime(self):
+        """Function to calculate the transformed Gibbs energy of reaction. Its stoichiometric combination of transformed Gibbs energy of formation of metabolites
 
-        """ Calculates standard Gibbs free energy of reactions (Needs to be transformed to pH and I to get transformed Gibbs free energy of reaction
-        
-        Returns:
-            float -- standard Gibbs free energy of reaction
+        Returns
+        -------
+        float
+            transformed reaction Gibbs energy
         """
-
         rxn_delG = 0
         for metabolite, stoic in iteritems(self.metabolites):
             rxn_delG += stoic * float(metabolite.delG_f)
