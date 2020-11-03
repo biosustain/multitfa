d16b9a49da4b5f5d017ed7230417eac037cdb56e
vishnumahamkali <vishnumahamkali@gmail.com>
2020-11-03 13:02:55 +1000

    doc string

--- multiTFA/core/compound.py
+++ multiTFA/core/compound.py
@@ -8,23 +8,20 @@ from equilibrator_api import Q_
 
 
 class Thermo_met(Metabolite):
-    """ Class representation of thermodynamic metabolite object
+    """ Class representation of thermodynamic metabolite object. It takes cobra Metabolite object as input and creates the Thermo_met object. Object is populated with properties required for thermodynamic calculations. We use equilibrator-api to do the Gibbs energy calculations.
     
-    Arguments:
-        Metabolite {cobra.core.Metabolite} -- Cobra metabolite object
+    Parameters
+        ----------
+        metabolite : cobra.core.Metabolite
+            cobra metabolite object
+        updated_model : cobra.core.Model, optional
+            cobra model, by default None
     
-    Attributes:
-        All the cobra metabolite attributes
-        Kegg_id: Kegg/Seed id of the metabolite
-        delG_f: standard formation energy of the compound
-        concentration_min: lb concentration
-        concentration_max: ub concentration
     """
 
     def __init__(
         self, metabolite, updated_model=None,
     ):
-
         self._model = updated_model
         self._reaction = set()
         do_not_copy_by_ref = {"_reaction", "_model"}
@@ -34,6 +31,13 @@ class Thermo_met(Metabolite):
 
     @property
     def concentration_variable(self):
+        """optlang variable for concentration of metabolite. This variable varies between lower and upper bounds of specified concentration. if not specified, uses generic concentration bounds.
+
+        Returns
+        -------
+        optlang.interface.variable
+            optlang interface variable for concentration of metabolite. if metabolite is not linked to model, then None
+        """
         if self.model is not None:
             conc_var = "lnc_{}".format(self.id)
             return self.model.variables[conc_var]
@@ -42,6 +46,13 @@ class Thermo_met(Metabolite):
 
     @property
     def concentration_min(self):
+        """lower bound of metabolite concentration. Units should be in Moles. If not specified, it either assumes compartment specific information (metabolite.model.compartment_info["c_min]) or uses 1e-5 M.
+
+        Returns
+        -------
+        float
+            metabolite concentration lower bound. returns input value or compartment specific value or 1e-5
+        """
         try:
             return self._concentration_min
         except AttributeError:
@@ -77,6 +88,13 @@ class Thermo_met(Metabolite):
 
     @property
     def concentration_max(self):
+        """Upper bound of metabolite concentration. Units should be in Moles. If not specified, it either assumes compartment specific information (metabolite.model.compartment_info["c_max]) or uses 2e-2 M.
+
+        Returns
+        -------
+        float
+            metabolite concentration upper bound. returns input value or compartment specific value or 2e-2
+        """
         try:
             return self._concentration_max
         except AttributeError:
@@ -112,6 +130,18 @@ class Thermo_met(Metabolite):
 
     @property
     def Kegg_id(self):
+        """External database identifier to match and retrieve thermodynamic properties from equilibrator-api. Will accept any identifier that equilibrator uses. Please refer to equilibrator-api documentation. Should use their corresponding format.
+
+        for example for atp
+        E.g: 
+        if bigg databse id: bigg.metabolite:atp
+            Kegg: kegg:C00002
+
+        Returns
+        -------
+        str
+            External database id for metabolite
+        """
         return self._Kegg_id
 
     @Kegg_id.setter
@@ -121,6 +151,13 @@ class Thermo_met(Metabolite):
 
     @property
     def delG_err_variable(self):
+        """optlang variable to represent the error on the formation energy of metabolite. Allowed to vary between 2 S.D from mean. 
+
+        Returns
+        -------
+        optlang.interface.variable
+            metabolite Gibbs energy error variable, if metabolite is not associated with model, then None
+        """
         if self.model is not None:
             err_var = "dG_err_{}".format(self.id)
             return self.model.variables[err_var]
@@ -129,6 +166,13 @@ class Thermo_met(Metabolite):
 
     @property
     def compound_vector(self):
+        """Metabolite component decomposition vector. Its useful for represent what compinents are present in the compound.
+
+        Returns
+        -------
+        np.ndarray
+            metabolite component decomposition vector
+        """
         try:
             return self._compound_vector
         except AttributeError:
@@ -137,6 +181,13 @@ class Thermo_met(Metabolite):
 
     @property
     def delG_f(self):
+        """Gibbs energy of formation of the metabolite in kJ/mol.
+
+        Returns
+        -------
+        float
+            Formation energy of metabolite in kJ/mol
+        """
         try:
             return self._delG_f
         except AttributeError:
@@ -149,6 +200,13 @@ class Thermo_met(Metabolite):
 
     @property
     def std_dev(self):
+        """standard deviation of compound Gibbs formation energy. 
+
+        Returns
+        -------
+        float
+            compound standard deviation of formation
+        """
         try:
             return self._std_dev
         except AttributeError:
@@ -171,6 +229,13 @@ class Thermo_met(Metabolite):
 
     @property
     def is_exclude(self):
+        """Function to check if metabolite needs to be excluded from thermodynamic analysis. if the compound component decomposition vector is all zeros then compound is excluded from thermodynamic analysis.
+
+        Returns
+        -------
+        Boolean
+            True if component decomposition vector is zeros, False otherwise.
+        """
         if self.is_proton:
             return False
         elif ~self.compound_vector.any():
@@ -180,7 +245,13 @@ class Thermo_met(Metabolite):
 
     @property
     def is_proton(self):
+        """Checks if the compound is proton, matching against inchikey
 
+        Returns
+        -------
+        Boolean
+            True if proton, False otherwise
+        """
         if self.equilibrator_accession:
             if self.equilibrator_accession.inchi_key == PROTON_INCHI_KEY:
                 return True
@@ -189,9 +260,15 @@ class Thermo_met(Metabolite):
 
     @property
     def equilibrator_accession(self):
+        """retrieves metabolite accession from equilibrator to get the termodynamic properties. We do this in batch in self.model,metabolite_equilibrator_accessions.
+
+        Returns
+        -------
+        equilibrator.compound
+            equilibrator compound object
+        """
         try:
             return self._equilibrator_accession
-            # print(self._equilibrator_accession.id)
         except AttributeError:
             self._equilibrator_accession = self.model.metabolite_equilibrator_accessions[
                 self.id
@@ -200,6 +277,15 @@ class Thermo_met(Metabolite):
 
     @property
     def std_dev_expression(self):
+        """optlang expression to represent the metabolite error in terms of components instead of formation energies.
+
+        expression = compound_vector @ variables
+
+        Returns
+        -------
+        sympy expression
+            error expression in terms of component variables
+        """
         if self.model._var_update:
             return (
                 self.compound_vector[np.nonzero(self.compound_vector)]
@@ -222,9 +308,6 @@ class Thermo_met(Metabolite):
                         Compound vector with index of the compound in training data (component contribution) or None
             
         """
-        # self._equilibrator_accession = api.get_compound(self.Kegg_id)
-        # print(self._equilibrator_accession.id)
-        # time.sleep(0.5)
         if self.equilibrator_accession:
             try:
                 rc_index = rc_compound_ids.index(self.equilibrator_accession.id)
