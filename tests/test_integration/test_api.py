8f8d3c16be19553b2772f2f7a7131d95dc4358c8
Moritz E. Beber <midnighter@posteo.net>
2020-11-30 11:19:56 +0100

    style: apply QA

--- tests/test_integration/test_api.py
+++ tests/test_integration/test_api.py
@@ -22,8 +22,10 @@ from importlib import import_module
 import pytest
 
 
-@pytest.mark.parametrize("public_module, symbol", [("multitfa", "show_versions"),
-                                                   ("multitfa.core", "tmodel")])
+@pytest.mark.parametrize(
+    "public_module, symbol",
+    [("multitfa", "show_versions"), ("multitfa.core", "tmodel")],
+)
 def test_public_api(public_module, symbol):
     """Expect the given public package interface."""
     public_module = import_module(public_module)
