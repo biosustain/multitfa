220997b904aaec3daf141eece8ea5e2269c7c488
vishnumahamkali <vishnumahamkali@gmail.com>
2021-01-17 13:37:55 +1000

    cutoff sampling options

--- src/multitfa/analysis/sampling.py
+++ src/multitfa/analysis/sampling.py
@@ -14,6 +14,7 @@ def cutoff_sampling(
     variable_list=None,
     min_growth=False,
     fraction_of_optim=0.9,
+    solver_name=None,
 ):
     """Implements the quadratic constraint using repeated sampling on the surface of ellipsoid. Exits when 100 consecutive samples represent better solution. We fix the component sphere variables lb & ub to the sampled covariance and solve the problem.
 
@@ -29,6 +30,8 @@ def cutoff_sampling(
         Boolean, to add minimum growth constraint or not, by default False
     fraction_of_optim : float, optional
         fraction of original growth/flux value, by default 0.9
+    solver_name : str, optional
+        preferred solver, if not specified and has more than one solver uses the same solver order as cobra, by default 0.9
 
     Returns
     -------
@@ -40,9 +43,11 @@ def cutoff_sampling(
     ValueError
         Initial check to see if model is feasible with given constraints
     """
-    model_copy = deepcopy(model_variability)
+    model_copy = deepcopy(model_variability)  # Copy the original model for sampling
+    model = preprocess_model(model_copy)  # Preprocess the model to add sphere variables
 
-    model = preprocess_model(model_copy)
+    if solver_name:
+        model.solver = solver_name
 
     # Retrieve small and large sphere variables
     small_sphere_vars = [
@@ -81,6 +86,13 @@ def cutoff_sampling(
 
     representative_ranges = DataFrame(
         np.zeros((len(variables), 2)), columns=["minimum", "maximum"]
+    )
+
+    Whole_ranges = DataFrame(
+        {
+            "minimum": Series(index=variables, data=np.empty(len(variables))),
+            "maximum": Series(index=variables, data=np.empty(len(variables))),
+        }
     )
 
     n_improvement, total_samples = (0, 0)
@@ -124,8 +136,10 @@ def cutoff_sampling(
             else:
                 n_improvement = n_improvement + 1
 
+            Whole_ranges = concat([Whole_ranges, tva_ranges], axis=1)
+
         lastone = tva_ranges
-    return representative_ranges, total_samples
+    return representative_ranges, Whole_ranges, total_samples
 
 
 def gev_sampling(
@@ -239,7 +253,7 @@ def gev_sampling(
         if tva_ranges.empty or tva_ranges.isnull().all()["maximum"]:
             total_samples = total_samples - 1
             continue
-        print(total_samples, tva_ranges)
+        # print(total_samples, tva_ranges)
         Whole_ranges = concat([Whole_ranges, tva_ranges], axis=1)
 
     for var in variables:
@@ -255,7 +269,7 @@ def gev_sampling(
         }
     )
 
-    return representative_ranges
+    return representative_ranges, Whole_ranges
 
 
 def sampling(
