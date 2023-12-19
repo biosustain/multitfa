e2a4a32a41f3618d793211ac327a1fb1f9faa315
vishnumahamkali <vishnumahamkali@gmail.com>
2023-12-19 11:49:14 +1000

    Fixing some bugs and active development

--- src/multitfa/util/thermo_constants.py
+++ src/multitfa/util/thermo_constants.py
@@ -12,12 +12,9 @@ DATA_DIR = Path(__file__).parent.parent / "data"
 covar_data = np.load(DATA_DIR / "component_data.npz")
 
 covariance = covar_data["covariance"]
-# cholesky_small = covar_data["cholesky_low"]
-# cholesky_high = covar_data["cholesky_big"]
-# chi2_value_small = stat.chi2.isf(q=0.05, df=cholesky_small.shape[1])
-# chi2_value_high = stat.chi2.isf(q=0.05, df=cholesky_high.shape[1])
 
-params = CCModelParameters.from_quilt()
+
+params = CCModelParameters.from_zenodo()
 rc_compound_ids = params.train_G.index.tolist()
 MSE_rc = params.MSE.at["rc", "MSE"]
 MSE_gc = params.MSE.at["gc", "MSE"]
@@ -31,7 +28,7 @@ mu = np.hstack([params.dG0_cc, params.dG0_gc[:Ng]])
 R = 8.31e-3  # "kJ / mol / K"
 FARADAY = 96.485  # "kJ / mol"
 Vmax = 1000
-K = 1e8
+K = 1e6
 default_T = 298.15  # , "K")
 default_I = 0.25  # , "M")
 default_pH = 7.0
